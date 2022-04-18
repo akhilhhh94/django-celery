433725ccec0a988ef08ab8226bdb90183f6dfdcd
akhilhhh94 <akhilhhh94@gmail.com>
2022-04-18 23:04:15 +0530

    sending email with email template

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 
 
+## Start redis server
+```shell
+docker run -p 8888:6379 --name some-redis -d redis redis-server --save 60 1 --loglevel warning
+```
+
 ## Start beat
 ```shell
 celery -A django_celery_example beat -l info
--- polls/tasks.py
+++ polls/tasks.py
@@ -2,7 +2,7 @@ from celery.decorators import task
 from celery.utils.log import get_task_logger
 from django.core.mail import send_mail
 from django.conf import settings
-
+from django.template.loader import render_to_string
 
 logger = get_task_logger(__name__)
 
@@ -18,4 +18,6 @@ def send_feedback_email(email, message):
     subject = 'Thank you for registering to our site'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
-    send_mail(subject, message, email_from)
+    msg_plain = render_to_string('email/email.txt', {'name': email})
+    msg_html = render_to_string('email/email.html', {'name': email})
+    send_mail(subject, msg_plain, email_from, recipient_list, html_message=msg_html)
